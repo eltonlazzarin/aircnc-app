ff8d20e9a6b32132945bc65bf011295526a629d9
eltonlazzarin <elton.lazzarin@outlook.com>
2019-10-04 20:32:21 -0300

    project files

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,10 +3,29 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
 const path = require('path');
+const socketio = require('socket.io');
+const http = require('http');
 
 require('dotenv').config();
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+  const { user_id } = socket.handshake.query;
+  
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
 
 app.use(cors());
 app.use(express.json());
@@ -22,6 +41,6 @@ mongoose.connect(process.env.MONGODB_URI, {
 
 const port = process.env.PORT || 3333;
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server started on port ${port}`)
 });
