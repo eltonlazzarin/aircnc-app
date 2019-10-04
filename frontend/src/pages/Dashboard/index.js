fb994fd8c6a1896994773a8a191f62dab08da2b0
eltonlazzarin <elton.lazzarin@outlook.com>
2019-10-04 20:31:14 -0300

    project files

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,11 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { Link } from 'react-router-dom';
+import socketio from 'socket.io-client';
 import api from '../../services/api';
 
 import './styles.css';
 
 export default function Dashboard() {
     const [spots, setSpots] = useState([]);
+    const [requests, setRequests] = useState([]);
+
+    const user_id = localStorage.getItem('user');
+    const socket = useMemo(() => socketio('http://192.168.0.104:3333', {
+        query: { user_id }
+    }), [user_id]);
+
+    useEffect(() => {
+        socket.on('booking_request', data => {
+            setRequests([...requests, data]);
+        })
+    }, [requests, socket]);
 
     useEffect(() => {
         async function loadSpots() {
@@ -20,8 +33,31 @@ export default function Dashboard() {
         loadSpots();
     }, []);
 
+    async function handleAccept(id) {
+        await api.post(`/bookings/${id}/approvals`);
+
+        setRequests(requests.filter(request => request._id !== id));
+    }
+
+    async function handleReject(id) {
+        await api.post(`/bookings/${id}/rejections`);
+
+        setRequests(requests.filter(request => request._id !== id));
+    }
+
     return (
         <>
+            <ul className="nofitications">
+                {requests.map(request => (
+                    <li key={request._id}>
+                        <p>
+                            <strong>{request.user.email}</strong> is requesting a reservation in <strong>{request.spot.company}</strong> to date: <strong>{request.date}</strong>
+                        </p>
+                        <button className="accept" onClick={() => handleAccept(request._id)}>ACCEPT</button> 
+                        <button className="reject" onClick={() => handleReject(request._id)}>REJECT</button> 
+                    </li>
+                ))}
+            </ul>
             <ul className="spot-list">
                 {spots.map(spot => (
                     <li key={spot._id}>
